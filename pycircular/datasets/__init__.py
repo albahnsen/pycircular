fb2934395cf79a8347419159bf032c87342f7fac
Alejandro Correa Bahnsen <al.bahnsen@gmail.com>
2023-01-12 16:01:21 -0500

    DOC add documentation and clean branch

--- pycircular/datasets/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-The :mod:`pycircular.datasets` module includes utilities to load datasets,
-including methods to load and fetch popular reference datasets. It also
-features some artificial data generators.
-"""
-
-from .base import load_transactions
-
-__all__ = ['load_transactions']
